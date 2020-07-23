8d517fd1a1780c49ca66d10147a19bc20e1c0e7b
Rodion <rodion.prosvirnin@bk.ru>
2020-07-23 11:18:47 +0300

    card view created. hstack, zstack, foreach loop. the end of the first section.

--- StanfordIOSSpring2020Memorize/ContentView.swift
+++ StanfordIOSSpring2020Memorize/ContentView.swift
@@ -10,12 +10,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack (content: {
-            RoundedRectangle(cornerRadius: 10.0)
-                .stroke()
-            Text("H")
-                .foregroundColor(.black)
-        })
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0)
+                    .stroke(lineWidth: 3)
+                Text("H")
+                    .foregroundColor(.black)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0)
+                    .fill()
+            }
+        }
     }
 }
 
